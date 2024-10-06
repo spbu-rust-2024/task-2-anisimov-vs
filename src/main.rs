26990575a4c1d571a0dbeb99adca87ad1af51fd6
Anisimov Vasiliy <st129629@student.spbu.ru>
2024-10-07 01:53:56 +0300

    main.rs: add comments and move algorithm to a separate function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::io;
-
+// Function to transform string to format. It needs to be done because it will reduce the calculation of an array for even lengths to an odd one
 fn transform(string: &str) -> Vec<u8> {
     let len_string = string.len();
 
-    let mut transformed_vector = Vec::with_capacity(2 * len_string + 3);
+    let mut transformed_vector = Vec::with_capacity(2 * len_string + 3); // 2 * n + 3 because we adding 2n+1 '#' and one '^' and '$' each
     transformed_vector.push(b'^');
     transformed_vector.push(b'#');
     for byte in string.as_bytes() {
@@ -14,26 +14,21 @@ fn transform(string: &str) -> Vec<u8> {
     transformed_vector
 }
 
-fn main() {
-    // Read input
-    let mut user_input: String = String::new();
-
-    io::stdin()
-        .read_line(&mut user_input)
-        .expect("Failed to read line");
-    // Transform input to format
-    let transformed = transform(&user_input);
-    // Initialize variables
+fn manacher(transformed: &Vec<u8>) -> Vec<usize> {
     let length = transformed.len();
     let mut palindrome_lengths = vec![0; length];
     let (mut center, mut right_boundary) = (0, 0);
     //Manacher's algorithm
     for current_index in 1..length - 1 {
-        //let mirror = 2 * center - current_index;
         if current_index < right_boundary {
-            palindrome_lengths[current_index] = usize::min(right_boundary - current_index, palindrome_lengths[2 * center - current_index]);
+            palindrome_lengths[current_index] = usize::min(
+                right_boundary - current_index,
+                palindrome_lengths[2 * center - current_index],
+            );
         }
-        while transformed[current_index + 1 + palindrome_lengths[current_index]] == transformed[current_index - 1 - palindrome_lengths[current_index]] {
+        while transformed[current_index + 1 + palindrome_lengths[current_index]]
+            == transformed[current_index - 1 - palindrome_lengths[current_index]]
+        {
             palindrome_lengths[current_index] += 1;
         }
 
@@ -42,17 +37,40 @@ fn main() {
             right_boundary = current_index + palindrome_lengths[current_index];
         }
     }
+    palindrome_lengths
+}
+
+fn longest_palindrome(palindrome_lengths: &Vec<usize>) -> (usize, usize) {
+    let (max_len, center_index) =
+        palindrome_lengths
+            .iter()
+            .enumerate()
+            .fold(
+                (0, 0),
+                |acc, (i, &len)| {
+                    if len > acc.0 {
+                        (len, i)
+                    } else {
+                        acc
+                    }
+                },
+            );
+    (max_len, center_index)
+}
+
+fn main() {
+    // Read input
+    let mut user_input: String = String::new();
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("Failed to read line");
+    // Transform input to format
+    let transformed = transform(&user_input);
+    //Manacher's algorithm
+    let palindrome_lengths = manacher(&transformed);
     // Find longest palindrome
-    let (max_len, center_index) = palindrome_lengths.iter().enumerate().fold((0, 0), |acc, (i, &len)| {
-        if len > acc.0 {
-            (len, i)
-        } else {
-            acc
-        }
-    });
+    let (max_len, center_index) = longest_palindrome(&palindrome_lengths);
     // Print longest palindrome
     let start = (center_index - max_len) / 2;
-
     println!("{}", &user_input[start..start + max_len as usize]);
-
 }
